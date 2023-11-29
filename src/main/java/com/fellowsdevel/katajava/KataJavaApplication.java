661e407881a74af5295e9af9a917fe0df36c8b0c
André Fellows <fellows.devel@gmail.com>
2023-11-29 20:30:52 -0300

    Initial commit

---
+++ src/main/java/com/fellowsdevel/katajava/KataJavaApplication.java
@@ -0,0 +1,13 @@
+package com.fellowsdevel.katajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fellowsdevel/katajava/KataJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fellowsdevel.katajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
